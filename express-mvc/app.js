16025928939f3795d2eb3a588a189b56fb24c7a8
Erikaa311 <160854903+Erikaa311@users.noreply.github.com>
2025-01-23 16:44:14 +0700

    latihan

--- express-mvc/app.js
+++ express-mvc/app.js
@@ -3,21 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-// load mongodb connection
+//load mongodb db connection
 require('./app_server/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var mhsRouter = require("./app_server/routes/mahasiswa");
+var housingRouter = require('./app_server/routes/housing');
 
-// pisah router
-var mhsController = require('./app_server/routes/mahasiswa');
+//pisah router
+var mhsRouter = require('./app_server/routes/mahasiswa');
 
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'ejs');
 
@@ -29,8 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// daftarkan route mahasiswa
-app.use('/mahasiswa', mhsController);
+app.use('/mahasiswa', mhsRouter);
+app.use('/housing', housingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
